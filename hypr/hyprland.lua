dbbdcff0f0cb13ef2b069df730fd3aa3d9f72387
sohamch08 <sohamchatterjee999@gmail.com>
2026-09-18 19:00:06 -0700

    night light hyprsunset

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -42,6 +42,7 @@ hl.on("hyprland.start", function()
     hl.exec_cmd("awww-daemon")
     hl.exec_cmd("gnome-keyring-daemon --start --components=secrets")
     hl.exec_cmd("swaync")
+    hl.exec_cmd("hyprsunset")
 end)
 
 
