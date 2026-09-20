8d7bff7b5875c4d19bcfc1c324c273b9b8ef9f9d
sohamch08 <sohamchatterjee999@gmail.com>
2026-09-20 16:53:56 -0700

    remove tray applications

--- hypr/modules/autostart.lua
+++ hypr/modules/autostart.lua
@@ -2,8 +2,8 @@
 ---- AUTOSTART ----
 -------------------
 hl.on("hyprland.start", function()
-    hl.exec_cmd("nm-applet")
-    hl.exec_cmd("blueman-applet")
+    -- hl.exec_cmd("nm-applet")
+    -- hl.exec_cmd("blueman-applet")
     hl.exec_cmd("waybar")
     hl.exec_cmd("awww-daemon")
     hl.exec_cmd("gnome-keyring-daemon --start --components=secrets")
