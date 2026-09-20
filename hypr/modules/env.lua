b5f98c6500f63cdaf7d785ad6b5bde4b7b985e7f
sohamch08 <sohamchatterjee999@gmail.com>
2026-09-20 04:04:51 -0700

    screenshot and binds

--- hypr/modules/env.lua
+++ hypr/modules/env.lua
@@ -23,3 +23,4 @@ hl.env("QT_AUTO_SCREEN_SCALE_FACTOR", "1")
 hl.env("QT_WAYLAND_DISABLE_WINDOWDECORATION", "1")
 hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
 
+hl.env("HYPRSHOT_DIR", "$HOME/Pictures/Screenshots")
